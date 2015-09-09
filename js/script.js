af2144bfbe0e948354ac03fc02257e53ce1cfd8a
Lily Guo <lily.724@hotmail.com>
2015-09-09 11:51:55 +1000

    change the speed of animation

--- js/script.js
+++ js/script.js
@@ -9,45 +9,14 @@ $(document).ready(function() {
 	});
 
 
-
-//main websites fly in
-	$(window).scroll(function() {
-		var y = $(window).scrollTop();
-		if (y >= 400) {
-			$('main #mySkills ul').addClass('animate');
-		}
-		if (y >= 800) {
-			$('main #travelSydney .col1').addClass('animate');
-			$('main #travelSydney .col2').addClass('animate');
-		}
-		if (y >1500) {
-			$('main #bc .col1').addClass('animate');
-			$('main #bc .col2').addClass('animate');
-		}
-		if (y >2000) {
-			$('main #tafeSydney .col1').addClass('animate');
-			$('main #tafeSydney .col2').addClass('animate');
-		}
-		if (y >2500) {
-			$('main #bcShopping .col1').addClass('animate');
-			$('main #bcShopping .col2').addClass('animate');
-		}
-		if (y >3000) {
-			$('main #cms .col1').addClass('animate');
-			$('main #cms .col2').addClass('animate');
-		}
-	});
-
-
-
 	//scroll down to the section
 	$(function() {
 		$('a').click(function() {
+			console.log(11111);
 			if(this.hash) {
 				var hash = this.hash.substr(1);
 				var $toElement = $('#' + hash );
-				console.log($toElement);
-				var toPosition = $toElement.position().top;
+				var toPosition = $toElement.offset().top;
 				$('body, html').animate({
 					scrollTop : toPosition
 				}, 2000, 'easeOutExpo');
@@ -62,7 +31,39 @@ $(document).ready(function() {
 		}
 	});
 
-	//
+
+//main websites fly in
+	$(window).scroll(function() {
+		var y = $(window).scrollTop();
+		if (y >= 800) {
+			$('main #mySkills ul').addClass('animate');
+		}
+		if (y >= 1100) {
+			$('main #travelSydney .col1').addClass('animate');
+			$('main #travelSydney .col2').addClass('animate');
+		}
+		if (y >1700) {
+			$('main #bc .col1').addClass('animate');
+			$('main #bc .col2').addClass('animate');
+		}
+		if (y >2300) {
+			$('main #tafeSydney .col1').addClass('animate');
+			$('main #tafeSydney .col2').addClass('animate');
+		}
+		if (y >2900) {
+			$('main #bcShopping .col1').addClass('animate');
+			$('main #bcShopping .col2').addClass('animate');
+		}
+		if (y >3400) {
+			$('main #cms .col1').addClass('animate');
+			$('main #cms .col2').addClass('animate');
+		}
+	});
+
+
+
+
+
 
 
 
